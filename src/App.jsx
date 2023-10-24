19b36aece0f4fc252154cf41cee8dd71da145b6b
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-10-24 16:28:10 +0000

    updates

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="">
+        <div className="">
+          <h1>hello world</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
